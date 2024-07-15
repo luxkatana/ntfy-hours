20ef8ac25f51dec9f837b7f617e3a2e18acec886
luxkatana <taseen.bibi@gmail.com>
2024-07-15 16:34:06 +0000

    add loop, update datetime now

--- main.py
+++ main.py
@@ -16,27 +16,25 @@ HEADERS = {
     'Content-Type': 'application/json',
 }
 def main():
-    now = datetime.now()
-
-    for shift in shifts:
-        if (now.weekday() + 1) == shift.weekday and (now.hour, now.minute) == shift.until:
-            data = {
-                'topic': 'luxkatanaworking',
-                'message': f'Shift klaar, begonnen vanaf 16:30 tot {now.hour}:{now.minute}',
-                'actions': [
-                    {
-                        'action': 'view',
-                        'label': 'Open',
-                        'url': f'https://thetrojanhorse3.pythonanywhere.com/?begin={now.hour}:{now.minute}'
-                    }
-                ]
-                
-            }
-            requests.post('https://ntfy.sh', headers=HEADERS, data=dumps(data))
-            break
-            
-
-    wait(60) # each minute check
+    while 1 == 1:
+        now = datetime.now()
+        for shift in shifts:
+            if (now.weekday() + 1) == shift.weekday and (now.hour, now.minute) == shift.until:
+                data = {
+                    'topic': 'luxkatanaworking',
+                    'message': f'Shift klaar, begonnen vanaf 16:30 tot {now.hour}:{now.minute}',
+                    'actions': [
+                        {
+                            'action': 'view',
+                            'label': 'Open',
+                            'url': f'https://thetrojanhorse3.pythonanywhere.com/?begin={now.hour}:{now.minute}'
+                        }
+                    ]
+                    
+                }
+                requests.post('https://ntfy.sh', headers=HEADERS, data=dumps(data))
+                break
+        wait(60) # each minute check
 
 if  __name__  ==  '__main__':
     main()
